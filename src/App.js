2775ee0a4e5fcbc030db0a82843a5923880ded29
NAVEENKUMAR2424 <naveenkumar978636@gmail.com>
2023-12-07 10:21:03 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,31 +1,94 @@
+import React from 'react';
 import './App.css';
-import {useFormik} from formmik;
+import { useFormik } from 'formik';
+import { Schema } from 
 
 
-const App=()=> {
-  const formik=useFormik
+function App() {
+  const {formik} = useFormik({
+    initialValues: {
+      username:'',
+      email: '',
+      password:'',
+      cpass:''
+    },
+    validationSchema: Schema
+  });
+
+
+
   return (
     <div className="App">
-      
-
       <h1>Login Form</h1>
-      <div class="textInputWrapper">
-    <input placeholder="Username" type="text" class="textInput"/>
-    </div>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="form__group field">
+          <input
+            type="text"
+            className="form__field"
+            placeholder="UserName"
+            required=""
+            name="username"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <label htmlFor="name" className="form__label">
+            UserName
+          </label>
+        </div>
 
-    <div class="textInputWrapper">
-    <input placeholder="Email" type="email" class="textInput"/>
-    </div>
+        <div className="form__group field">
+          <input
+            type="email"
+            className="form__field"
+            placeholder="Email"
+            required=""
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <label htmlFor="email" className="form__label">
+            Email
+          </label>
+        </div>
 
-    <div class="textInputWrapper">
-    <input placeholder="Password" type="Password" class="textInput"/>
-    </div>
+        <div className="form__group field">
+          <input
+            type="password"
+            className="form__field"
+            placeholder="Password"
+            required=""
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <label htmlFor="password" className="form__label">
+            Password
+          </label>
+        </div>
 
-    <div class="textInputWrapper">
-    <input placeholder="Confirm Password" type="password" class="textInput"/>
-    </div>
+        <div className="form__group field">
+          <input
+            type="password"
+            className="form__field"
+            placeholder="Confirm Password"
+            required=""
+            name="cpass"
+            value={formik.values.cpass}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <label htmlFor="confirmPassword" className="form__label">
+            Confirm Password
+          </label>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-  }
+}
 
 export default App;
