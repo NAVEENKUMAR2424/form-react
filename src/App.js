54d674facfaeb690a40e9e05e761dbce82a1bdda
NAVEENKUMAR2424 <naveenkumar978636@gmail.com>
2023-12-07 10:14:19 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import {useFormik} from formmik;
 
-function App() {
+
+const App=()=> {
+  const formik=useFormik
   return (
     <div className="App">
       
+
+      <h1>Login Form</h1>
+      <div class="textInputWrapper">
+    <input placeholder="Username" type="text" class="textInput"/>
+    </div>
+
+    <div class="textInputWrapper">
+    <input placeholder="Email" type="email" class="textInput"/>
+    </div>
+
+    <div class="textInputWrapper">
+    <input placeholder="Password" type="Password" class="textInput"/>
+    </div>
+
+    <div class="textInputWrapper">
+    <input placeholder="Confirm Password" type="password" class="textInput"/>
+    </div>
     </div>
   );
-}
+  }
 
 export default App;
